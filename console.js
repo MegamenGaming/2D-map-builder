d888c9c80d8367449e96411312d7f58ea1d1d8c6
apeggs2000 <lancepeters@Alexs-MacBook-Pro.local>
2014-03-19 13:30:53 -0700

    Add my code for console.js

--- console.js
+++ console.js
@@ -1,1 +1,29 @@
- 
+//declare the variables from the HTML file
+var consoleDiv = document.getElementById("console");
+var consoleBox = document.getElementById("consoleBox");
+
+//the jQuery to hide/show the console
+$(document).ready(function() {
+  //hide the console at startup
+  $('#console').hide();
+  //a variable used later on to re-hide the console
+  var consoleLoaded = false;
+  
+  //key code
+  var evt = $('body').keypress(function(event){
+    var keyPressed = event.keyCode;
+    //if the user presses '/', the console will show
+    if (keyPressed === 47 && consoleLoaded === false) {
+      $('#console').show();
+      //when the console is shown, this becomes true
+      consoleLoaded = true;
+    } //endif
+    //if the console is shown, then pressing '/' will hide it
+    elseif (keyPressed === 47 && consoleLoaded === true) {
+      $('#console').hide();
+        //this is now hidden and set to false
+        consoleLoaded = false;
+    } //endif
+  });
+
+});
